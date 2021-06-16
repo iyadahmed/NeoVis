eb22c6ee14d5932af2267f63a7023fe0d62df55b
sam <sam@kali>
2021-06-16 13:29:42 -0400

    Revert "Revert "Several changes""
    This reverts commit a8e4b11051a64230e2cd2ebb91b4654173004b69.

---
+++ src/common.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+
+#include "common.h"
+
+int get_int_pair(const IntPair *pairs, const size_t num_pairs, int value,
+                 int default_value) {
+  for (size_t i = 0; i < num_pairs; i++) {
+    if (pairs->a == value) {
+      return pairs->b;
+    }
+    if (pairs->b == value) {
+      return pairs->a;
+    }
+  }
+  return default_value;
+}
