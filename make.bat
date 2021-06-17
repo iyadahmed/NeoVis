54920e6e86491a7cc43279f31bc26a5e6592fe43
Iyad Ahmed <cgonfire@outlook.com>
2021-06-17 09:51:37 +0200

    remove make.bat

--- make.bat
+++
@@ -1,9 +0,0 @@
-set "srcdir=src"
-set "builddir=build"
-
-pushd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-popd
-
-if not exist %builddir% mkdir %builddir%
-cl "%srcdir%\main.c" C:\libjpeg-turbo\lib\turbojpeg-static.lib /Fo"%builddir%\main.o" /Fe"%builddir%\neovis.exe" /I C:\libjpeg-turbo\include
