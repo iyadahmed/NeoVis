796ee1cea2bc5b8247c36d886aec999a789e5731
Iyad Ahmed <cgonfire@outlook.com>
2021-06-21 22:12:51 +0200

    added a stack data structure

--- src/common/common.c
+++ src/common/common.c
@@ -1,6 +1,16 @@
 #include <stddef.h>
+#include <stdlib.h>
 
 #include "common.h"
+
+// free memory allocated to stack and its items
+// also sets the pointer to NULL
+#define FREE_STACK(stack_ptr)                                                  \
+  {                                                                            \
+    free(stack->items);                                                        \
+    free(stack);                                                               \
+    stack = NULL;                                                              \
+  }
 
 int get_int_pair(const IntPair *pairs, const size_t num_pairs, int value,
                  int default_value) {
@@ -13,4 +23,47 @@ int get_int_pair(const IntPair *pairs, const size_t num_pairs, int value,
     }
   }
   return default_value;
+}
+
+Stack *create_stack(int maxsize) {
+  Stack *stack = malloc(sizeof(Stack));
+  stack->maxsize = maxsize;
+  stack->top = -1;
+  stack->items = malloc(maxsize * sizeof(int));
+  return stack;
+}
+
+bool is_full(Stack *stack) { return stack->top == stack->maxsize - 1; }
+bool is_empty(Stack *stack) { return stack->top == -1; }
+
+// returns 0 on sucess, -1 if stack is full
+int push(Stack *stack, int value) {
+  if (is_full(stack)) {
+    return -1;
+  }
+
+  stack->items[stack->top] = value;
+  stack->top++;
+  return 0;
+}
+
+// returns 0 on sucess, -1 if stack is empty
+int pop(Stack *stack, int *out) {
+  if (is_empty(stack)) {
+    return -1;
+  }
+
+  *out = stack->items[stack->top];
+  stack->top--;
+  return 0;
+}
+
+// returns 0 on sucess, -1 if stack is empty
+int peek(Stack *stack, int *out) {
+  if (is_empty(stack)) {
+    return -1;
+  }
+
+  *out = stack->items[stack->top];
+  return 0;
 }
--- src/common/common.h
+++ src/common/common.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 // goes to "bail" label
@@ -10,7 +11,20 @@ typedef struct IntPair {
   int a, b;
 } IntPair;
 
+typedef struct Stack {
+  int top;
+  int maxsize;
+  int *items;
+} Stack;
+
 int get_int_pair(const IntPair *pairs, const size_t num_pairs, int value,
                  int default_value);
+
+Stack *create_stack(int maxsize);
+bool is_full(Stack *stack);
+bool is_empty(Stack *stack);
+int push(Stack *stack, int x);
+int pop(Stack *stack, int *out);
+int peek(Stack *stack, int *out);
 
 #endif /* COMMON_H */
