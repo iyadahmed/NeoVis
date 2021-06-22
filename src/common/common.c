243758c3088dcaa1bf8c171a6d1530d9e3aa143e
Iyad Ahmed <cgonfire@outlook.com>
2021-06-22 21:43:48 +0200

    Remove stack implementation

--- src/common/common.c
+++ src/common/common.c
@@ -3,15 +3,6 @@
 
 #include "common.h"
 
-// free memory allocated to stack and its items
-// also sets the pointer to NULL
-#define FREE_STACK(stack_ptr)                                                  \
-  {                                                                            \
-    free(stack_ptr->items);                                                    \
-    free(stack_ptr);                                                           \
-    stack_ptr = NULL;                                                          \
-  }
-
 int get_int_pair(const IntPair *pairs, const size_t num_pairs, int value,
                  int default_value) {
   for (size_t i = 0; i < num_pairs; i++) {
@@ -23,47 +14,4 @@ int get_int_pair(const IntPair *pairs, const size_t num_pairs, int value,
     }
   }
   return default_value;
-}
-
-Stack *create_stack(size_t maxsize) {
-  Stack *stack = malloc(sizeof(Stack));
-  stack->maxsize = maxsize;
-  stack->top = -1;
-  stack->items = malloc(maxsize * sizeof(int));
-  return stack;
-}
-
-bool is_full(Stack *stack) { return stack->top == stack->maxsize - 1; }
-bool is_empty(Stack *stack) { return stack->top == -1; }
-
-// returns 0 on sucess, -1 if stack is full
-int push(Stack *stack, int value) {
-  if (is_full(stack)) {
-    return -1;
-  }
-
-  stack->items[stack->top] = value;
-  stack->top++;
-  return 0;
-}
-
-// returns 0 on sucess, -1 if stack is empty
-int pop(Stack *stack, int *out) {
-  if (is_empty(stack)) {
-    return -1;
-  }
-
-  *out = stack->items[stack->top];
-  stack->top--;
-  return 0;
-}
-
-// returns 0 on sucess, -1 if stack is empty
-int peek(Stack *stack, int *out) {
-  if (is_empty(stack)) {
-    return -1;
-  }
-
-  *out = stack->items[stack->top];
-  return 0;
 }
--- src/common/common.h
+++ src/common/common.h
@@ -11,20 +11,7 @@ typedef struct IntPair {
   int a, b;
 } IntPair;
 
-typedef struct Stack {
-  int top;
-  int maxsize;
-  int *items;
-} Stack;
-
 int get_int_pair(const IntPair *pairs, const size_t num_pairs, int value,
                  int default_value);
-
-Stack *create_stack(size_t maxsize);
-bool is_full(Stack *stack);
-bool is_empty(Stack *stack);
-int push(Stack *stack, int x);
-int pop(Stack *stack, int *out);
-int peek(Stack *stack, int *out);
 
 #endif /* COMMON_H */
